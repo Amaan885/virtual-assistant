a45192bd6a15860260deed5820834d74cc3d53d0
Amaan885 <amaanansari0003@gmail.com>
2025-06-16 02:41:59 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
 import { useContext } from 'react'
-import { userDataContex } from './contex/userContex'
+import { userDataContex } from './contex/UserContex'
 import Home from './pages/Home'
 import Customize2 from './pages/Customize2'
 
